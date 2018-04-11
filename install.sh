d7249234c2ff5243ea00a588a963208ca7d50ba1
bmihaljevic <boki.mihaljevic@gmail.com>
2018-04-11 09:45:45 +0200

    updates

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt install git
+sudo apt-get install r-base r-base-dev
+sudo apt install -f
+sudo apt install libssl-dev
+sudo apt install libssh2-1-dev
+sudo apt install libcurl4-openssl-dev
+sudo apt install libxml2-dev
+sudo apt install xclip 
+sudo apt install vim
+sudo apt install keepassx
+sudo apt install pandoc
